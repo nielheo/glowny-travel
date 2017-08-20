1244f64a5a19e619b937d20cb1df5e8c78d1fff6
niel <niel.heo@gmail.com>
2017-08-20 12:47:19 +0700

    add glowny-travel-native

---
+++ glowny-travel-native/App.js
@@ -0,0 +1,13 @@
+//import SimpleStack from './SimpleStack'
+//import TabNavigatorModule from './TabNavigatorModule'
+import DrawerNavigatorModule from './DrawerNavigatorModule'
+
+import {
+  AppRegistry,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+
+
+AppRegistry.registerComponent('GlownyTravelNative', () => DrawerNavigatorModule);
